620cd19ffe8f86468a1e1b39cb13eeffd68c2108
Harsh Choubey <harshchoubay759@gmail.com>
2025-08-15 16:31:30 +0530

    project complete

---
+++ src/constants.js
@@ -0,0 +1,284 @@
+// Skills Section Logo's
+import htmlLogo from "./assets/tech_logo/html.png";
+import cssLogo from "./assets/tech_logo/css.png";
+import sassLogo from "./assets/tech_logo/sass.png";
+import javascriptLogo from "./assets/tech_logo/javascript.png";
+import reactjsLogo from "./assets/tech_logo/reactjs.png";
+import angularLogo from "./assets/tech_logo/angular.png";
+import reduxLogo from "./assets/tech_logo/redux.png";
+import nextjsLogo from "./assets/tech_logo/nextjs.png";
+import tailwindcssLogo from "./assets/tech_logo/tailwindcss.png";
+import gsapLogo from "./assets/tech_logo/gsap.png";
+import materialuiLogo from "./assets/tech_logo/materialui.png";
+import bootstrapLogo from "./assets/tech_logo/bootstrap.png";
+import springbootLogo from "./assets/tech_logo/springboot.png";
+import nodejsLogo from "./assets/tech_logo/nodejs.png";
+import expressjsLogo from "./assets/tech_logo/express.png";
+import mysqlLogo from "./assets/tech_logo/mysql.png";
+import mongodbLogo from "./assets/tech_logo/mongodb.png";
+import firebaseLogo from "./assets/tech_logo/firebase.png";
+import cLogo from "./assets/tech_logo/c.png";
+import cppLogo from "./assets/tech_logo/cpp.png";
+import javaLogo from "./assets/tech_logo/java.png";
+import pythonLogo from "./assets/tech_logo/python.png";
+import typescriptLogo from "./assets/tech_logo/typescript.png";
+import gitLogo from "./assets/tech_logo/git.png";
+import githubLogo from "./assets/tech_logo/github.png";
+import vscodeLogo from "./assets/tech_logo/vscode.png";
+import postmanLogo from "./assets/tech_logo/postman.png";
+import mcLogo from "./assets/tech_logo/mc.png";
+import figmaLogo from "./assets/tech_logo/figma.png";
+import netlifyLogo from "./assets/tech_logo/netlify.png";
+import vercelLogo from "./assets/tech_logo/vercel.png";
+import postgreLogo from "./assets/tech_logo/postgre.png";
+import csharpLogo from "./assets/tech_logo/csharp.png";
+import nsitLogo from "./assets/education_logo/nsit.png";
+
+import gparariaLogo from "./assets/education_logo/gp-araria.jpeg";
+
+import chattyLogo from "./assets/work_logo/chatty.png";
+import onlineMusicLogo from "./assets/work_logo/onlinne-music.png";
+import ecommerceLogo from "./assets/work_logo/e-commerce.png";
+import foodLogo from "./assets/work_logo/foodLogo.png";
+import gameLogo from "./assets/work_logo/rock-game.png";
+import catLogo from "./assets/work_logo/cat-fact.png";
+import portfolioLogo from "./assets/work_logo/portfolioLogo.png";
+
+import aagazLogo from "./assets/company_logo/aagaz-logo.png";
+import nielitLogo from "./assets/company_logo/nielit-logo.png";
+import commusyncLogo from "./assets/company_logo/commusync-logo.png";
+
+export const SkillsInfo = [
+  {
+    title: "Frontend",
+    skills: [
+      { name: "HTML", logo: htmlLogo },
+      { name: "CSS", logo: cssLogo },
+      { name: "SASS", logo: sassLogo },
+      { name: "JavaScript", logo: javascriptLogo },
+      { name: "React JS", logo: reactjsLogo },
+      { name: "Angular", logo: angularLogo },
+      { name: "Redux", logo: reduxLogo },
+      { name: "Next JS", logo: nextjsLogo },
+      { name: "Tailwind CSS", logo: tailwindcssLogo },
+      { name: "GSAP", logo: gsapLogo },
+      { name: "Material UI", logo: materialuiLogo },
+      { name: "Bootstrap", logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      { name: "Springboot", logo: springbootLogo },
+      { name: "Node JS", logo: nodejsLogo },
+      { name: "Express JS", logo: expressjsLogo },
+      { name: "MySQL", logo: mysqlLogo },
+      { name: "MongoDB", logo: mongodbLogo },
+      { name: "Firebase", logo: firebaseLogo },
+      { name: "PostgreSQL", logo: postgreLogo },
+    ],
+  },
+  {
+    title: "Languages",
+    skills: [
+      { name: "C", logo: cLogo },
+      { name: "C++", logo: cppLogo },
+      { name: "Java", logo: javaLogo },
+      { name: "Python", logo: pythonLogo },
+      { name: "C-Sharp", logo: csharpLogo },
+      { name: "JavaScript", logo: javascriptLogo },
+      { name: "TypeScript", logo: typescriptLogo },
+    ],
+  },
+  {
+    title: "Tools",
+    skills: [
+      { name: "Git", logo: gitLogo },
+      { name: "GitHub", logo: githubLogo },
+      { name: "VS Code", logo: vscodeLogo },
+      { name: "Postman", logo: postmanLogo },
+      { name: "Compass", logo: mcLogo },
+      { name: "Vercel", logo: vercelLogo },
+      { name: "Netlify", logo: netlifyLogo },
+      { name: "Figma", logo: figmaLogo },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: nielitLogo,
+    role: "Data Science Intern",
+    company: "NIELIT PATNA",
+    date: "Nov 2023 - Dec 2023",
+    desc: "I successfully completed a 6-week internship in Data Science using Python Programming at NIELIT Patna, a premier institute under the Ministry of Electronics and Information Technology (MeitY), Government of India The internship was project-based and emphasized practical application over theory. I worked on mini-projects and assignments that simulated real data-driven scenarios, helping me strengthen both my analytical and programming skills. This experience enhanced my ability to derive meaningful insights from complex data and reinforced my interest in pursuing a career in data science and analytics..",
+    skills: [
+      "Python",
+      "SQL",
+      "Panda",
+      "NumPy",
+      "Excel",
+      "PyTorch",
+      "TensorFlow",
+    ],
+  },
+  {
+    id: 1,
+    img: aagazLogo,
+    role: "MERN STACK Intern",
+    company: "Aagaz Training Center",
+    date: "June 2025 - July 2025",
+    desc: "I completed a one-month internship in MERN Stack Web Development at Aagaz Training Center, where I gained practical experience in building full-stack web applications using MongoDB, Express.js, React.js, and Node.js. During the internship, I worked on designing user interfaces with React, developing backend APIs with Express and Node, and integrating MongoDB for database operations. I also learned how to implement routing, manage application state, and use Git for version control. This internship helped me understand the complete workflow of web application development and strengthened my skills in both frontend and backend technologies within the MERN stack..",
+    skills: [
+      "Redux",
+      "JavaScript",
+      "Tailwind CSS",
+      "REACT",
+      "MONGODB",
+      "NODE JS",
+      "EXPRESS",
+    ],
+  },
+  {
+    id: 2,
+    img: commusyncLogo,
+    role: "Full Stack Intern",
+    company: "Commusync",
+    date: "Aug 2025 - Oct 2025",
+    desc: "Worked as a Frontend Developer Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Bootstrap, and Material UI. Collaborated with the design team to translate wireframes and prototypes from Figma into interactive, user-friendly web pages.",
+    skills: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "Bootstrap",
+      "Figma",
+      "Material UI",
+      "React",
+      "MonngoDb",
+      "Express",
+      "Node Js",
+      "Tailwind CSS",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: nsitLogo,
+    school: "Netaji Subhas Institute of Technology",
+    date: "Sept 2023 - July 2026",
+    grade: "8.23 CGPA",
+    desc: "I am currently pursuing a Bachelor of Technology (B.Tech) degree in Computer Science and Engineering from Netaji Subhas Institute of Technology (NSIT), Patna, with an expected completion in July 2026. I began the program in September 2023, and throughout the course, I have been building a strong foundation in core areas such as programming, data structures, algorithms, computer networks, database management systems, and software development. The curriculum is designed to align with current industry trends and includes practical labs, project work, and exposure to emerging technologies. My time at NSIT Patna has helped me develop both technical skills and problem-solving abilities, preparing me for a career in software engineering and technology.",
+    degree: "Bachelor Of Technology . (B.TECH)",
+  },
+  {
+    id: 1,
+    img: gparariaLogo,
+    school: "Government Polytechnic Araria",
+    date: "Sept 2020 - Aug 2023",
+    grade: "8.75 CGPA",
+    desc: "I have completed Diploma in Mechanical Engineering from Government Polytechnic, Araria. During this program, I gained a strong foundation in the principles of mechanical engineering, including subjects such as engineering mechanics, thermodynamics, fluid mechanics, machine design, and manufacturing processes. The course emphasized both theoretical knowledge and practical application through lab work, workshops, and industrial training. This diploma has equipped me with essential technical skills, problem-solving abilities, and a solid understanding of core mechanical systems, preparing me for further studies and professional opportunities in the field of engineering.",
+    degree: "Diploma in Mechanical engg",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Online Chat App",
+    description:
+      "I developed an online chat application using the MERN stack, which includes MongoDB, Express.js, React.js, and Node.js. This real-time chat app allows users to create accounts, join chat rooms, and send instant messages to others. I implemented user authentication and managed data storage with MongoDB to ensure smooth and secure communication. The front-end was built using React.js to provide a responsive and interactive user interface, while the back-end API was developed with Node.js and Express.js to handle message routing and user management. This project helped me strengthen my skills in full-stack development, real-time data handling, and building scalable web applications.",
+    image: chattyLogo,
+    tags: ["REACT", "MONGODB", "EXPRESS", "NODE JS", "API", "Java-Script"],
+    github: "https://github.com/Harsh7209/Chat-App.git",
+    webapp: "https://fullstack-chat-app-6-opmz.onrender.com",
+  },
+  {
+    id: 1,
+    title: "Online Music Streaming Platform",
+    description:
+      "This online music platform is a full-stack web application developed using the MERN stack, comprising MongoDB, Express.js, React.js, and Node.js. The platform enables users to stream high-quality music, explore a diverse library of songs categorized by genre, artist, and popularity, and manage personalized playlists within a clean and responsive user interface. It features secure user authentication implemented with JWT and bcrypt, along with a custom-built RESTful API to handle all backend operations and database interactions. The frontend, developed in React, offers smooth navigation and real-time updates, while the backend ensures efficient data handling and scalability. Music files and metadata are integrated with cloud storage solutions for optimal performance. The project demonstrates a complete end-to-end music streaming experience with modern web technologies..",
+
+    image: onlineMusicLogo,
+    tags: [
+      "React JS",
+      "Node.js",
+      "MongoDB",
+      "Express",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Stripe",
+      "Cloudinary",
+    ],
+    github: "https://github.com/Harsh7209/Online-Music--Streaming-Website.git",
+    webapp: "https://full-stack-spotify-clone-jjcr.onrender.com",
+  },
+  {
+    id: 2,
+    title: "E-Commerce Platform",
+    description:
+      "This e-commerce platform is a fully functional web application built using the MERN stack, which includes MongoDB, Express.js, React.js, and Node.js. The platform allows users to browse products across various categories, view detailed product information, add items to a shopping cart, and securely complete purchases through an integrated checkout system. It features user authentication and role-based access control, enabling regular users to manage their orders and administrators to manage products, categories, and users through a dedicated admin dashboard. The frontend offers a responsive and intuitive shopping experience, while the backend handles API requests, order processing, and secure data management. MongoDB is used for storing user, product, and order data, with RESTful APIs enabling seamless communication between the client and server. This project showcases the full lifecycle of an online shopping experience using modern web technologies..",
+    image: ecommerceLogo,
+    tags: [
+      "React JS",
+      "Node.js",
+      "MongoDB",
+      "Express",
+      "Framer Motion",
+      "Motion Div",
+      "Tailwind CSS",
+      "JavaScript",
+      "Stripe",
+      "Cloudinary",
+    ],
+    github: "https://github.com/Harsh7209/E-Commerce.git",
+    webapp: "https://e-commerce-hcgn.onrender.com",
+  },
+  {
+    id: 3,
+    title: "Online Food Delivery App",
+    description:
+      "This online food delivery app is a full-stack web application developed using the MERN stack, consisting of MongoDB, Express.js, React.js, and Node.js. The platform allows users to browse a variety of restaurants and food items, add meals to their cart, place real-time orders, and track their delivery status through a clean and responsive user interface. It includes secure user authentication and authorization, enabling customers to manage their profiles and order history, while restaurant owners or admins can manage menus, orders, and delivery statuses through a dedicated dashboard. The backend handles all business logic, API routing, and database operations, ensuring smooth order processing and data management. MongoDB is used to store dynamic data such as users, restaurants, menus, and orders. The application delivers a seamless user experience, combining convenience and functionality, and showcases a complete food ordering and delivery system using modern web technologies.",
+    image: foodLogo,
+    tags: [
+      "React JS",
+      "Node.js",
+      "NPM",
+      "Validation",
+      "Express",
+      "MONGODB",
+      "Cloudinary",
+      "RazorPay",
+      "JavaScript",
+    ],
+  },
+  {
+    id: 4,
+    title: "Rock Paper Scissor Game",
+    description:
+      "This Rock, Paper, Scissors game is a simple and interactive web application developed using HTML, CSS, and JavaScript. The game allows users to play against the computer by selecting one of the three options—rock, paper, or scissors—while the computer randomly generates its own choice. The interface is designed with a clean and responsive layout using HTML and CSS, while JavaScript handles the core game logic, including user input, random computer selection, result comparison, and score tracking. It provides instant feedback to the player and displays the outcome of each round, making it both fun and engaging. This project showcases the fundamentals of DOM manipulation, conditional logic, and event handling in JavaScript.",
+    image: gameLogo,
+    tags: ["JavaScript", "Chrome Extension", "HTML", "CSS"],
+    github: "https://github.com/Harsh7209/Rock-Paper-Scissor-Game.git",
+  },
+  {
+    id: 5,
+    title: "Cat Fact Generator",
+    description:
+      "The Cat Fact Generator is a fun and lightweight web application designed to fetch and display random cat facts to users. Built using HTML, CSS, and JavaScript, the app interacts with a public API to retrieve interesting and often surprising facts about cats with each click of a button. The user interface is simple, responsive, and visually appealing, providing a smooth and engaging experience. JavaScript is used to handle API requests, update the DOM dynamically, and manage user interactions. This project demonstrates how to work with external APIs, implement asynchronous JavaScript using fetch, and build a user-friendly interface for delivering dynamic content.",
+    image: catLogo,
+    tags: ["HTML", "CSS", "JavaScript", "Framer Motion", "Tailwind CSS"],
+    github: "https://github.com/Harsh7209/Cat-Fact-Generator.git",
+  },
+  {
+    id: 6,
+    title: "PortFolio Website",
+    description:
+      "I developed a modern and responsive portfolio website using React and Tailwind CSS to showcase my skills, projects, and professional journey. The site features a clean and intuitive UI, smooth navigation, and mobile-friendly design to ensure an optimal viewing experience across devices. Dynamic components in React enhance performance and interactivity, while Tailwind CSS ensures a consistent and visually appealing design with custom styling. The portfolio includes sections for an “About Me,” skills, project showcases with detailed descriptions, and a contact form for easy communication.",
+    image: portfolioLogo,
+    tags: ["ReactJS", "Tailwind CSS", "Framer Motion", "JavaScript"],
+  },
+];
