2ab01111fe8819ada9ec52e3d5617ebb625df915
Harsh Choubey <harshchoubay759@gmail.com>
2025-10-10 12:43:55 +0530

    updated resume.

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -53,7 +53,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1pnKgU4PUqJIKa1UeGXgIfuqz_h7nso88/view?usp=sharing"
+            href="https://drive.google.com/file/d/1Qv-HecwesFGRCiyZbh8InyrAjD1i4eOY/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
